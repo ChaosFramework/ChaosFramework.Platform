583f98157166a286e8d82dab823c91700b5d423e
InsanityCode <56584762+InsanityCode@users.noreply.github.com>
2026-06-27 14:53:09 +0200

    add EnumerateMonitors and some proper API specification

--- source/PlatformContext.cs
+++ source/PlatformContext.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace ChaosFramework.Platform
 {
     public delegate void Overhead();
@@ -7,13 +9,32 @@ namespace ChaosFramework.Platform
         /// <summary> If not null, this describes how the platform can be used to target the OpenGL API. </summary>
         GlContext glContext { get; }
 
-        Window CreateWindow(string title);
-        Fullscreen CreateFullscreen(string title);
+        /// <summary>
+        ///     Enumerates all currently connected logical monitors,
+        ///     ordered by their index as assigned by the operating system.
+        /// </summary>
+        /// <remarks>
+        ///     There is no guarantee that when queried multiple times,
+        ///     the logical monitors are represented by the same <see cref="Monitor"/> instances.
+        /// </remarks>
+        IEnumerable<Monitor> EnumerateMonitors();
 
-        /// <summary> Raised when the process receives a signal from the OS that it should be terminated. </summary>
+        /// <summary> Creates a windowed PresentationContext. </summary>
+        /// <param name="title"> The title of the operating system level "window". </param>
+        /// <returns> The created windowed PresentationContext. </returns>
+        Window CreateWindow(string title);
+
+        /// <summary> Creates a full screen PresentationContext on the provided monitor. </summary>
+        /// <param name="title"> The title of the operating system level "window". </param>
+        /// <param name="monitor"> The monitor to present on. </param>
+        /// <exception cref="ArgumentException">Thrown if the provided monitor is of an unsupported type.</exception>
+        /// <returns> The created fullscreen PresentationContext. </returns>
+        Fullscreen CreateFullscreen(string title, Monitor monitor);
+
+        /// <summary> Raised when the process receives a signal from the operating system that it should be terminated. </summary>
         event System.Action Terminate;
 
         /// <summary> Processes incoming operating system level events. </summary>
-        Overhead messageQueue {get;}
+        Overhead messageQueue { get; }
     }
 }
